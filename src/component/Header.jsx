f7cbe28c9b73a172982e815421c12e7cb7abd7d5
Prashant Kumar <programmerkrprashant@gmail.com>
2025-03-07 19:05:41 +0530

    added style to nav

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router'
 const Header = () => {
   return (
     <>
-      <header>
+      <header className='bg-[#FBFDFC] m-auto w-full max-w-[1344px] h-[56px] flex justify-between items-center px-6 border-b border-gray-300'>
         <h3>DoIt</h3>
         <nav>
           <ul>
