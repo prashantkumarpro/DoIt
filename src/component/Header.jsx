4dc9a61e3473dbe8e16661dd0ef404ad3917d0a8
Prashant Kumar <programmerkrprashant@gmail.com>
2025-03-07 23:29:58 +0530

    header styled

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,20 +1,46 @@
+import {
+  RiGridFill,
+  RiMenuFill,
+  RiMoonLine,
+  RiSearchLine,
+  RiTodoLine
+} from '@remixicon/react'
 import React from 'react'
-import { NavLink } from 'react-router'
+import { Link } from 'react-router'
 
 const Header = () => {
   return (
     <>
       <header className='bg-[#FBFDFC] m-auto w-full max-w-[1344px] h-[56px] flex justify-between items-center px-6 border-b border-gray-300'>
-        <h3>DoIt</h3>
-        <nav>
-          <ul>
+        <div className='flex gap-6 items-center'>
+          <Link>
+            {' '}
+            <RiMenuFill className='size-6' />
+          </Link>
+          <h3 className='text-[#3F9142] text-xl flex items-center justify-center gap-0.5'>
+            <RiTodoLine className='size-6' />
+            DoIt{' '}
+          </h3>
+        </div>
+        <nav className=''>
+          <ul className='flex items-center justify-center gap-6'>
             <li>
-              {' '}
-              <NavLink to='/'>Search here</NavLink>{' '}
+              <Link>
+                <RiSearchLine className='size-6' />
+              </Link>
+            </li>
+            <li>
+              <Link>
+                {' '}
+                <RiGridFill className='size-6' />
+              </Link>
             </li>
             <li>
               {' '}
-              <NavLink to='/allTasks'>AllTask</NavLink>{' '}
+              <Link>
+                {' '}
+                <RiMoonLine className='size-6' />
+              </Link>
             </li>
           </ul>
         </nav>
--- src/component/SideNav.jsx
+++ src/component/SideNav.jsx
@@ -3,28 +3,30 @@ import { NavLink } from 'react-router'
 
 const SideNav = () => {
   return (
-    <aside>
-      <h3>DoIt</h3>
-      <nav>
-        <ul>
-          <li>
-            {' '}
-            <NavLink to='/'>All Tasks</NavLink>{' '}
-          </li>
-          <li>
-            {' '}
-            <NavLink to='/'>Today</NavLink>{' '}
-          </li>
-          <li>
-            {' '}
-            <NavLink to='/'>Today</NavLink>{' '}
-          </li>
-          <li>
-            {' '}
-            <NavLink to='/'>Today</NavLink>{' '}
-          </li>
-        </ul>
-      </nav>
+    <aside className='w-56 h-screen px-6 py-6'>
+      <div className='part1 size-24 bg-black rounded-full flex items-center justify-center m-auto'>
+        <figure className='w-24 bg-amber-950 rounded-full'>
+          <figcaption>HI, ABCD</figcaption>
+        </figure>
+      </div>
+      <ul className='mt-6 mx-6 px-6 h-screen flex flex-col gap-3 bg-[#EEF6EF]'>
+        <li>
+          {' '}
+          <NavLink to='/'>All Tasks</NavLink>{' '}
+        </li>
+        <li>
+          {' '}
+          <NavLink to='/'>Today</NavLink>{' '}
+        </li>
+        <li>
+          {' '}
+          <NavLink to='/'>Today</NavLink>{' '}
+        </li>
+        <li>
+          {' '}
+          <NavLink to='/'>Today</NavLink>{' '}
+        </li>
+      </ul>
     </aside>
   )
 }
